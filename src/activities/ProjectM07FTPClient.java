8ccbdc75ec4f463bec0f326041acc838265a84b4
pablosanchez1999 <48524810+pablosanchez1999@users.noreply.github.com>
2019-03-13 16:47:17 +0100

    Add files via upload

---
+++ src/activities/ProjectM07FTPClient.java
@@ -0,0 +1,95 @@
+package activities;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Point;
+import javax.swing.JToolBar;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JButton;
+import javax.swing.JMenu;
+import javax.swing.JSeparator;
+import javax.swing.JCheckBoxMenuItem;
+
+public class ProjectM07FTPClient extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	private JPanel contentPane;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ProjectM07FTPClient frame = new ProjectM07FTPClient();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public ProjectM07FTPClient() {
+		setLocation(new Point(200, 170));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = new JPanel();
+		contentPane.setPreferredSize(new Dimension(700, 500));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+
+		JMenuBar menuBar = new JMenuBar();
+		contentPane.add(menuBar);
+
+		JMenu menuVer = new JMenu("Ver");
+		menuBar.add(menuVer);
+
+		JMenuItem menuItemVerActualizar = new JMenuItem("Actualizar");
+		menuVer.add(menuItemVerActualizar);
+
+		JSeparator separator = new JSeparator();
+		menuVer.add(separator);
+
+		JCheckBoxMenuItem checkBarraYEstados = new JCheckBoxMenuItem("Barra y estado de lista de archivos");
+		checkBarraYEstados.setSelected(true);
+		menuVer.add(checkBarraYEstados);
+
+		JSeparator separator_1 = new JSeparator();
+		menuVer.add(separator_1);
+
+		JCheckBoxMenuItem checkBarraHerramientas = new JCheckBoxMenuItem("Barra de herramientas");
+		checkBarraHerramientas.setSelected(true);
+		menuVer.add(checkBarraHerramientas);
+
+		JCheckBoxMenuItem checkBarraConexionRapida = new JCheckBoxMenuItem("Barra de conexión ráapida");
+		checkBarraConexionRapida.setSelected(true);
+		menuVer.add(checkBarraConexionRapida);
+
+		JCheckBoxMenuItem checkRegistroMensajes = new JCheckBoxMenuItem("Registro de mensajes");
+		checkRegistroMensajes.setSelected(true);
+		menuVer.add(checkRegistroMensajes);
+
+		JCheckBoxMenuItem checkArbolLocal = new JCheckBoxMenuItem("Árbol de directorio local");
+		checkArbolLocal.setSelected(true);
+		menuVer.add(checkArbolLocal);
+
+		JCheckBoxMenuItem checkArbolRemoto = new JCheckBoxMenuItem("Árbol de directorio remoto");
+		checkArbolRemoto.setSelected(true);
+		menuVer.add(checkArbolRemoto);
+
+		JCheckBoxMenuItem checkColaTransferencia = new JCheckBoxMenuItem("Cola de transferencia");
+		checkColaTransferencia.setSelected(true);
+		menuVer.add(checkColaTransferencia);
+
+		pack();
+
+	}
+
+}
