811e69e869ada3dc59bb58521cdbfdf44a8a5207
ausias <ausias@h4-25.xiam.cat>
2019-03-27 18:22:51 +0100

    Cambios Settings

--- src/activities/ProjectM07FTPSettingsPanel.java
+++ src/activities/ProjectM07FTPSettingsPanel.java
@@ -19,6 +19,8 @@ import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.JButton;
+import javax.swing.border.LineBorder;
 
 public class ProjectM07FTPSettingsPanel extends JPanel {
 	public ProjectM07FTPSettingsPanel() throws URISyntaxException, HeadlessException, AWTException {
@@ -29,63 +31,66 @@ public class ProjectM07FTPSettingsPanel extends JPanel {
 		panelMain.setBounds(0, 0, 10, 10);
 		add(panelMain);
 		panelMain.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Select page:");
 		lblNewLabel.setBounds(12, 12, 113, 15);
 		panelMain.add(lblNewLabel);
-		
-		  Icon leafIcon = new ImageIcon("leaf.gif");
-		  Icon openIcon = new ImageIcon("open.gif");
-		  Icon closedIcon = new ImageIcon("closed.gif");
-		
+
+		Icon leafIcon = new ImageIcon(ProjectM07FTPClient.class.getResource(""));
+	
+
 		JTree tree = new JTree();
-		tree.setModel(new DefaultTreeModel(
-			new DefaultMutableTreeNode("Settings") {
-				{
-					DefaultMutableTreeNode node_1;
-					DefaultMutableTreeNode node_2;
-					node_1 = new DefaultMutableTreeNode("Connection");
-						node_2 = new DefaultMutableTreeNode("FTP");
-							node_2.add(new DefaultMutableTreeNode("Active mode"));
-							node_2.add(new DefaultMutableTreeNode("Passive mode\t\t"));
-							node_2.add(new DefaultMutableTreeNode("FTP Proxy"));
-						node_1.add(node_2);
-						node_1.add(new DefaultMutableTreeNode("SFTP"));
-						node_1.add(new DefaultMutableTreeNode("Generic proxy"));
-					add(node_1);
-					node_1 = new DefaultMutableTreeNode("Transfers");
-						node_1.add(new DefaultMutableTreeNode("FTP: File Types"));
-						node_1.add(new DefaultMutableTreeNode("File exists action"));
-					add(node_1);
-					node_1 = new DefaultMutableTreeNode("Interface");
-						node_1.add(new DefaultMutableTreeNode("Passwords"));
-						node_1.add(new DefaultMutableTreeNode("Themes"));
-						node_1.add(new DefaultMutableTreeNode("Data/time format"));
-						node_1.add(new DefaultMutableTreeNode("Filesize format"));
-						node_1.add(new DefaultMutableTreeNode("File lists"));
-					add(node_1);
-					add(new DefaultMutableTreeNode("Language"));
-					node_1 = new DefaultMutableTreeNode("File editing");
-						node_1.add(new DefaultMutableTreeNode("Filetype associations"));
-					add(node_1);
-					add(new DefaultMutableTreeNode("Updates"));
-					add(new DefaultMutableTreeNode("Logging"));
-					add(new DefaultMutableTreeNode("Debug"));
-				}
+		tree.setRootVisible(false);
+		tree.setBorder(new LineBorder(new Color(0, 0, 0)));
+		tree.setModel(new DefaultTreeModel(new DefaultMutableTreeNode("Settings") {
+			{
+				DefaultMutableTreeNode node_1;
+				DefaultMutableTreeNode node_2;
+				node_1 = new DefaultMutableTreeNode("Connection");
+				node_2 = new DefaultMutableTreeNode("FTP");
+				node_2.add(new DefaultMutableTreeNode("Active mode"));
+				node_2.add(new DefaultMutableTreeNode("Passive mode\t\t"));
+				node_2.add(new DefaultMutableTreeNode("FTP Proxy"));
+				node_1.add(node_2);
+				node_1.add(new DefaultMutableTreeNode("SFTP"));
+				node_1.add(new DefaultMutableTreeNode("Generic proxy"));
+				add(node_1);
+				node_1 = new DefaultMutableTreeNode("Transfers");
+				node_1.add(new DefaultMutableTreeNode("FTP: File Types"));
+				node_1.add(new DefaultMutableTreeNode("File exists action"));
+				add(node_1);
+				node_1 = new DefaultMutableTreeNode("Interface");
+				node_1.add(new DefaultMutableTreeNode("Passwords"));
+				node_1.add(new DefaultMutableTreeNode("Themes"));
+				node_1.add(new DefaultMutableTreeNode("Data/time format"));
+				node_1.add(new DefaultMutableTreeNode("Filesize format"));
+				node_1.add(new DefaultMutableTreeNode("File lists"));
+				add(node_1);
+				add(new DefaultMutableTreeNode("Language"));
+				node_1 = new DefaultMutableTreeNode("File editing");
+				node_1.add(new DefaultMutableTreeNode("Filetype associations"));
+				add(node_1);
+				add(new DefaultMutableTreeNode("Updates"));
+				add(new DefaultMutableTreeNode("Logging"));
+				add(new DefaultMutableTreeNode("Debug"));
 			}
-		));
-	    DefaultTreeCellRenderer renderer = (DefaultTreeCellRenderer) tree.getCellRenderer();
-	    renderer.setLeafIcon(leafIcon);
-	    renderer.setClosedIcon(closedIcon);
-	    renderer.setOpenIcon(openIcon);
-
-		tree.setBounds(12, 27, 243, 451);
+		}));
+		DefaultTreeCellRenderer renderer = (DefaultTreeCellRenderer) tree.getCellRenderer();
+		renderer.setLeafIcon(leafIcon);
+		
+		tree.setBounds(12, 27, 243, 344);
 		panelMain.add(tree);
-		
 
-		
 		JPanel panel = new JPanel();
 		panel.setBounds(267, 12, 461, 466);
 		panelMain.add(panel);
+		
+		JButton btnNewButton = new JButton("Accept");
+		btnNewButton.setBounds(12, 383, 243, 25);
+		panelMain.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Cancel");
+		btnNewButton_1.setBounds(12, 420, 243, 25);
+		panelMain.add(btnNewButton_1);
 	}
 }
